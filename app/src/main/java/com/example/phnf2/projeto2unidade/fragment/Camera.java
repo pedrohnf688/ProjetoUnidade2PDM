808f69d0839ba5ca20932b3438ee6c152d02080e
pedrohnf688 <pedrohnf@ufrn.edu.br>
2018-11-11 15:16:39 -0200

    Versao 3

--- app/src/main/java/com/example/phnf2/projeto2unidade/fragment/Camera.java
+++ app/src/main/java/com/example/phnf2/projeto2unidade/fragment/Camera.java
@@ -1,19 +1,38 @@
 package com.example.phnf2.projeto2unidade.fragment;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.phnf2.projeto2unidade.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Camera extends Fragment {
+public class Camera extends Fragment  {
 
+    private final static int ACESSAR_GALERIA = 1;
+    private ImageView imagem;
+    Button galeria;
+    Button tirarFoto;
+    private final static int PERMISSAO_REQUEST = 2;
+    private final static int CAPTURAR_FOTO = 3;
 
     public Camera() {
         // Required empty public constructor
@@ -26,7 +45,79 @@ public class Camera extends Fragment {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_camera, container, false);
 
-        return v;
+        imagem = v.findViewById(R.id.camera_preview);
+        galeria = v.findViewById(R.id.selecione_captura);
+        tirarFoto = v.findViewById(R.id.capturarfoto);
+
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,ACESSAR_GALERIA);
+
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
+
+        tirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent,CAPTURAR_FOTO);
+                }
+            }
+        });
+
+
+            return v;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Acesso a Galeria de fotos
+        if (resultCode == RESULT_OK && requestCode == ACESSAR_GALERIA) {
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getActivity().getContentResolver().query(selectedImage,filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            imagem.setImageBitmap(thumbnail);
+        }
+           //Tirar foto
+        if (requestCode == CAPTURAR_FOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagem.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSAO_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //A permissão foi concedida. Pode continuar
+            } else {
+                // A permissão foi negada. Precisa ver o que deve ser desabilitado
+            }
+            return;
+        }
+    }
 }
